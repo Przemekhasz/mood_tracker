dbedeec51277578a6ba93019596131d3ed7a39f7
Przemysław Tarapacki <tarapackiprzemek45@gmail.com>
2021-12-23 22:31:30 +0100

    fix repo

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  final String _title = 'Śledź swój nastruj';
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: _title,
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      home: MyApp(),
+    );
+  }
+}
